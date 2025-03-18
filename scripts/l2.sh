680a72b5e387aa414b09417af38f6d951054dc4f
barnard33-233 <htang0203@gmail.com>
2025-03-18 20:51:40 +0800

    l2 changes save 250318

--- scripts/l2.sh
+++ scripts/l2.sh
@@ -2,7 +2,7 @@ BINARY_PATH=/mnt/out/bin/
 IMAGE_PATH=$BINARY_PATH/Image-guest
 ROOTFS_PATH=$BINARY_PATH/br/rootfs.cpio
 
-qemu-system-aarch64 -serial stdio -monitor telnet:127.0.0.1:1234,server,nowait \
+qemu-system-aarch64 -serial /dev/hvc1 -monitor /dev/hvc2 \
     -M 'virt,acpi=off,gic-version=3' -cpu host -enable-kvm -smp 1 -m 512M -overcommit 'mem-lock=on' \
     -M 'confidential-guest-support=rme0' \
     -object 'rme-guest,id=rme0,measurement-algo=sha512,migration-cap=dev' \
